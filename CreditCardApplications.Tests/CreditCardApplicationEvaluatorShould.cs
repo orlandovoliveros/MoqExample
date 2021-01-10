cf57d6ca70c717a5b871ab5c208b674151ede0fa
Orlando Oliveros <oroli@orsted.dk>
2021-01-10 12:44:07 +0800

    Adding Custom Error Message

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -173,7 +173,7 @@ namespace CreditCardApplications.Tests
 
             sut.Evaluate(application);
 
-            mockValidator.Verify(x => x.IsValid(It.IsAny<string>()));
+            mockValidator.Verify(x => x.IsValid(It.IsAny<string>()), "Frequent flyer number should be validated.");
         }
     }
 }
